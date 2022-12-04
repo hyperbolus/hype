3ce61930f7824f881eb21d33b2807aa9ded04ff2
IDEA <mrmajesticsandwich@gmail.com>
2022-12-04 15:41:26 -0600

    Update new thread page to new layout

--- resources/js/Pages/Threads/Create.vue
+++ resources/js/Pages/Threads/Create.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue'
+import CommonLayout from '@/Layouts/CommonLayout.vue'
 import {Link, useForm, usePage} from '@inertiajs/inertia-vue3';
 import Input from "@/Jetstream/Input.vue";
 import PostPad from "@/Components/PostPad.vue";
@@ -8,7 +8,7 @@ import route from "ziggy-js";
 import Button from "@/Jetstream/Button.vue";
 
 const props = defineProps({
-    forum_id: Number
+    forum_id: [Number, String]
 })
 
 const form = useForm({
@@ -17,7 +17,7 @@ const form = useForm({
 })
 
 const postData = ref({
-    body: '',
+    body: '<p></p>',
     signature: !(!usePage().props.auth || !usePage().props.user.signature),
     rich: false,
 })
@@ -30,7 +30,7 @@ const submit = () => {
 }
 </script>
 <template>
-    <app-layout title="Home">
+    <common-layout title="Home">
         <template #breadcrumbs>
             <Link :href="route('forums.index')">Forums</Link>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
@@ -39,23 +39,18 @@ const submit = () => {
             <span>Create Thread</span>
         </template>
         <div class="y lg:max-w-5xl xl:max-w-6xl w-full gap-4 p-4">
-            <h2 class="mx-2 font-bold text-2xl">Create a New Thread</h2>
-            <div class="y box">
-                <label class="y">
-                    <span class="my-1">Title</span>
-                    <Input type="text" v-model="form.title" placeholder="Required"/>
-                </label>
-            </div>
-            <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+            <ul v-if="$page.props.errors.length > 0" class="mt-3 list-disc list-inside text-sm text-red-600">
                 <li v-for="(error, key) in $page.props.errors" :key="key">
                     {{ error }}
                 </li>
             </ul>
+            <h2 class="mx-2 font-bold text-2xl">Create a New Thread</h2>
+            <Input class="p-2" type="text" v-model="form.title" placeholder="Title (Required)"/>
             <PostPad v-model="postData"/>
             <div class="x justify-center gap-2">
                 <Button onclick="alert('wip feature')">Save Draft</Button>
                 <Button :disabled="form.processing" @click="submit">Post Thread</Button>
             </div>
         </div>
-    </app-layout>
+    </common-layout>
 </template>
