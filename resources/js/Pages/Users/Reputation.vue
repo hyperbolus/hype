41f30a8d2840fb738878287bca667129d1137c61
IDEA <mrmajesticsandwich@gmail.com>
2022-12-07 19:06:07 -0600

    Update reputation log to common layout and move pagination location

--- resources/js/Pages/Users/Reputation.vue
+++ resources/js/Pages/Users/Reputation.vue
@@ -1,10 +1,9 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue'
+import CommonLayout from '@/Layouts/CommonLayout.vue'
 import {Link} from '@inertiajs/inertia-vue3';
 import Username from "@/Components/Username.vue";
 import route from 'ziggy-js'
 import Pagination from "@/Components/Pagination.vue";
-import Avatar from "@/Components/Avatar.vue";
 
 const props = defineProps({
     profile: Object,
@@ -12,7 +11,7 @@ const props = defineProps({
 })
 </script>
 <template>
-    <app-layout title="Home">
+    <common-layout title="Reputation Log">
         <template #breadcrumbs>
             <Link :href="route('users.show', profile.id)">{{ profile.name }}'s Profile</Link>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
@@ -22,10 +21,8 @@ const props = defineProps({
         </template>
         <div class="flex lg:max-w-5xl xl:max-w-6xl w-full gap-4 p-4">
             <div class="flex flex-col space-y-2 md:w-3/4">
-                <div class="flex justify-between items-center">
-                    <h2 class="font-bold text-2xl">{{ profile.name }}'s Reputation</h2>
-                    <Pagination :list="reps"/>
-                </div>
+                <h2 class="font-bold text-2xl">{{ profile.name }}'s Reputation</h2>
+                <Pagination :list="reps"/>
                 <div class="box !px-0 !py-0 divide-y dark:divide-neutral-700">
                     <div v-for="rep in reps.data" class="x items-center py-4 px-4">
                         <div class="x items-center font-bold p-2 w-8 h-8 rounded mr-4 bg-neutral-200 dark:bg-neutral-800" :class="{'!bg-red-500': rep.reputation < 0, '!bg-green-500': rep.reputation > 0, '!text-white': rep.reputation !== 0}">
@@ -39,5 +36,5 @@ const props = defineProps({
                 </div>
             </div>
         </div>
-    </app-layout>
+    </common-layout>
 </template>
