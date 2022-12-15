c67853cecd036b7f3ad698aaf40d70c38d547de1
IDEA <mrmajesticsandwich@gmail.com>
2022-12-14 18:48:27 -0600

    idk why this wasnt included in big mess commit

---
+++ app/Models/Content/CrowdTagVote.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Content;
+
+use App\Models\IdeHelperLevelTagVote;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin IdeHelperLevelTagVote
+ */
+class CrowdTagVote extends Model
+{
+    use HasFactory;
+}
