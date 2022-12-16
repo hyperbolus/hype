9b5627bcc86b05ffbc53e07b5d3a4404e0bbb05d
IDEA <mrmajesticsandwich@gmail.com>
2022-12-16 01:05:20 -0600

    Move out global navbar

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -10,6 +10,7 @@ import {useToggle} from "@vueuse/core";
 import Avatar from "@/Components/Avatar.vue";
 import bg from "@/../images/wellrestedbg.jpg"
 import Tooltip from "@/Components/Tooltip.vue";
+import NetworkNav from "@/Components/NetworkNav.vue";
 
 const props = defineProps({
     title: String,
@@ -40,18 +41,7 @@ const toggleDark = () => {
 <template>
     <div ref="base" class="y transition-colors items-center text-neutral-700 dark:text-neutral-300 bg-neutral-200 dark:bg-neutral-800 min-h-screen">
         <Head><title>{{ title }}</title></Head>
-        <span class="hidden w-full px-1 py-0.5 x-text-center font-bold text-xs bg-indigo-400 dark:bg-indigo-600">DEV ENVIRONMENT &bull; 127.0.0.1 &bull; <a href="//phpmyadmin" class="underline">PhpMyAdmin</a> &bull; <Link :href="route('system.home')" class="underline">Control Panel</Link> &bull; You've been cunked</span>
-        <div class="x z-30 transition-colors justify-center w-full bg-neutral-300 dark:bg-neutral-900">
-            <div class="hidden md:flex px-2 items-center divide-x divide-neutral-200 dark:divide-neutral-800 lg:max-w-5xl xl:max-w-6xl w-full text-xs">
-                <a class="px-2 py-1 transition-colors bg-neutral-200 dark:bg-neutral-800 border-l dark:border-neutral-800">Geometry Dash</a>
-                <Tooltip class="px-2 py-1 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800" :caret="false" message="SpinShare is not affiliated with Hyperbolus websites">
-                    <a href="https://spinsha.re">Spin Rhythm</a>
-                </Tooltip>
-                <a href="https://soundodger-community.com" class="px-2 py-1 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">Soundodger</a>
-                <a href="https://userlevels.com/snail" class="px-2 py-1 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">Would You Snail?</a>
-                <a href="https://userlevels.com/impossible" class="px-2 py-1 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800 !border-r border-neutral-200 dark:border-neutral-800">The Impossible Game 2</a>
-            </div>
-        </div>
+        <NetworkNav/>
         <div class="y items-center z-20 transition-colors w-full py-4 bg-neutral-200 dark:bg-neutral-800">
             <div class="x px-2 justify-between lg:max-w-5xl xl:max-w-6xl w-full">
                 <div class="x items-center space-x-4">
