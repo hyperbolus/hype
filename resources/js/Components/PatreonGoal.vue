176d26ca6a6e5a96b97bd3db58794890b884ebd9
IDEA <mrmajesticsandwich@gmail.com>
2022-12-15 22:20:05 -0600

    transition colors

--- resources/js/Components/PatreonGoal.vue
+++ resources/js/Components/PatreonGoal.vue
@@ -6,12 +6,12 @@ const goal = ref(usePage().props.value.app.stats.patreon.included[0].attributes)
 </script>
 <template>
     <div class="y text-sm rounded overflow-clip shadow">
-        <div class="x justify-between px-2 py-2 bg-white dark:bg-neutral-900 bg-opacity-60 dark:bg-opacity-60">
+        <div class="x transition-colors justify-between px-2 py-2 bg-white dark:bg-neutral-900 bg-opacity-60 dark:bg-opacity-60">
             <a class="font-bold px-2 text-neutral-100 rounded-full" style="background-color:#FF424D;" href="https://patreon.com/seebeyond" target="_blank">Patreon</a>
             <span>Monthly Funding Goal</span>
         </div>
-        <div class="px-2 py-1 bg-white dark:bg-neutral-900 bg-opacity-40 dark:bg-opacity-40">
-            <div class="bg-white px-1 dark:bg-neutral-900 bg-opacity-40 dark:bg-opacity-40 text-center font-bold text-lg py-1 rounded-full w-full">
+        <div class="px-2 transition-colors py-1 bg-white dark:bg-neutral-900 bg-opacity-40 dark:bg-opacity-40">
+            <div class="transition-colors bg-white px-1 dark:bg-neutral-900 bg-opacity-40 dark:bg-opacity-40 text-center font-bold text-lg py-1 rounded-full w-full">
                 <div class="bg-cyan-400 text-center font-bold text-lg py-1 px-1 rounded-full" :style="`width: ${goal.completed_percentage}%;`"></div>
             </div>
             <div class="px-2 py-1 x text-xs justify-between">
@@ -19,6 +19,6 @@ const goal = ref(usePage().props.value.app.stats.patreon.included[0].attributes)
                 <span>$0 of ${{ goal.amount_cents / 100 }} per month</span>
             </div>
         </div>
-        <p class="px-2 py-1 x bg-white dark:bg-neutral-900 bg-opacity-60 dark:bg-opacity-60">{{ goal.description }}</p>
+        <p class="px-2 py-1 transition-colors x bg-white dark:bg-neutral-900 bg-opacity-60 dark:bg-opacity-60">{{ goal.description }}</p>
     </div>
 </template>
