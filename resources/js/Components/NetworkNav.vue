51c920af1ec24c0f4da8f0aa9eb6e8bd25f86f4e
IDEA <mrmajesticsandwich@gmail.com>
2022-12-19 18:00:10 -0600

    remove spinshare from sister sites

--- resources/js/Components/NetworkNav.vue
+++ resources/js/Components/NetworkNav.vue
@@ -6,9 +6,6 @@ import Tooltip from "@/Components/Tooltip.vue";
     <div class="x z-30 transition-colors justify-center w-full bg-neutral-300 dark:bg-neutral-900">
         <div class="hidden md:flex px-2 items-center divide-x divide-neutral-200 dark:divide-neutral-800 lg:max-w-5xl xl:max-w-6xl w-full text-xs">
             <a class="px-2 py-1 transition-colors bg-neutral-200 dark:bg-neutral-800 border-l dark:border-neutral-800">Geometry Dash</a>
-            <Tooltip class="px-2 py-1 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800" :caret="false" message="SpinShare is not affiliated with Hyperbolus websites">
-                <a href="https://spinsha.re">Spin Rhythm</a>
-            </Tooltip>
             <a href="https://soundodger-community.com" class="px-2 py-1 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">Soundodger</a>
             <a href="https://userlevels.com/snail" class="px-2 py-1 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800">Would You Snail?</a>
             <a href="https://userlevels.com/impossible" class="px-2 py-1 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800 !border-r border-neutral-200 dark:border-neutral-800">The Impossible Game 2</a>
