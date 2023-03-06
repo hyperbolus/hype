b9d172adc539ffff34eb42b335feaf52658222d1
0x1DEA <seebeyondedm@gmail.com>
2023-03-06 00:08:11 -0600

    Fix z-ordering on footer for layout

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -2,13 +2,13 @@
 import {Link} from '@inertiajs/vue3'
 </script>
 <template>
-    <div class="y p-4 transition-colors items-center w-full bg-neutral-100 dark:bg-neutral-900">
+    <div class="y p-4 z-10 transition-colors items-center w-full bg-neutral-100 dark:bg-neutral-900">
         <div class="px-4 lg:max-w-5xl xl:max-w-6xl w-full">
             <div class="flex justify-between items-center w-full text-neutral-400 dark:text-neutral-500">
                 <div>
                     <a href="#">&copy; Hyperbolus 2022</a>
                 </div>
-                <div class="hidden md:flex bg-neutral-800 py-0.5 px-2 rounded text-sm">
+                <div class="hidden md:flex transition-colors bg-neutral-200 dark:bg-neutral-800 py-0.5 px-2 rounded text-sm">
                     <template v-if="true /*&& (bg === bgsrc)*/">
                         <span>Featured Artwork by <span class="cursor-pointer underline">overdefo</span></span>
                     </template>
