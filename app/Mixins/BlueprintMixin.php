8cf6b8ae0241421cd30ea914196a11dc0cb84f44
0x1DEA <seebeyondedm@gmail.com>
2023-03-07 11:12:05 -0600

    add approved() blueprint macro

--- app/Mixins/BlueprintMixin.php
+++ app/Mixins/BlueprintMixin.php
@@ -7,16 +7,25 @@ use Illuminate\Database\Schema\Blueprint;
 /**
  * @mixin Blueprint
  */
-class BlueprintMixin {
-    public function intMorphs(string $name): void {
+class BlueprintMixin
+{
+    public function intMorphs(string $name): void
+    {
         $this->foreignId($name . '_id');
         $this->unsignedSmallInteger($name . '_type');
         $this->index([$name . '_id', $name . '_type']);
     }
 
-    public function nullableIntMorphs(string $name): void {
+    public function nullableIntMorphs(string $name): void
+    {
         $this->foreignId($name . '_id')->nullable();
         $this->unsignedSmallInteger($name . '_type')->nullable();
         $this->index([$name . '_id', $name . '_type']);
+    }
+
+    public function approved(): void
+    {
+        $this->timestamp('approved_at')->nullable();
+        $this->foreignId('approved_by_id')->nullable();
     }
 }
