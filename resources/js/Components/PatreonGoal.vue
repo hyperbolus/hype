55d009836500c3db5afc844eea74fc5b4e68dfe7
0x1DEA <seebeyondedm@gmail.com>
2023-06-13 18:10:25 -0500

    Patreon API changes, set goal to patron count for now

--- resources/js/Components/PatreonGoal.vue
+++ resources/js/Components/PatreonGoal.vue
@@ -2,22 +2,25 @@
 import {useStatisticsStore} from "@/stores/statistics.ts";
 
 const goal = useStatisticsStore().patreon
+const patronsGoal = 20;
 </script>
 <template>
-    <div class="y text-sm rounded overflow-clip shadow">
-        <div class="x transition-colors justify-between px-2 py-2 bg-white dark:bg-ui-900">
-            <a class="font-bold px-2 text-neutral-100 rounded-full" style="background-color:#FF424D;" href="https://patreon.com/hyperbolus" target="_blank">Patreon</a>
-            <span>Monthly Funding Goal</span>
-        </div>
-        <div v-if="goal" class="px-2 transition-colors py-1 bg-white dark:bg-ui-900">
-            <div class="transition-colors bg-white px-1 dark:bg-ui-800 text-center font-bold text-lg py-1 rounded-full w-full">
-                <div :class="goal.completed_percentage >= 100 ? 'bg-amber-50 shadow-[0_0px_6px_4px_rgba(200,120,40,0.75)]' : 'bg-cyan-400'" class="text-center font-bold text-lg py-1 px-1 rounded-full" :style="`width: ${Math.min(goal.completed_percentage, 100)}%;`"></div>
+    <div class="y text-sm rounded-lg overflow-clip shadow">
+        <template v-if="!goal.error">
+            <div class="x transition-colors justify-between px-2 py-2 bg-white dark:bg-ui-900">
+                <a class="font-bold px-2 text-neutral-100 rounded-full" style="background-color:#FF424D;" href="https://patreon.com/hyperbolus" target="_blank">Patreon</a>
+                <span>Monthly Goal</span>
             </div>
-            <div class="px-2 py-1 x text-xs justify-between">
-                <span>{{goal.completed_percentage}}%</span>
-                <span>${{ ((goal.amount_cents / 100) * (goal.completed_percentage / 100)).toFixed(2) }} of ${{ goal.amount_cents / 100 }} per month</span>
+            <div class="px-2 transition-colors py-1 bg-white dark:bg-ui-900">
+                <div class="transition-colors bg-white px-1 dark:bg-ui-800 text-center font-bold text-lg py-1 rounded-full w-full">
+                    <div :class="goal.patron_count >= patronsGoal ? 'bg-amber-50 shadow-[0_0px_6px_4px_rgba(200,120,40,0.75)]' : 'bg-cyan-400'" class="text-center font-bold text-lg py-1 px-1 rounded-full" :style="`width: ${Math.min(goal.patron_count / patronsGoal * 100, 100)}%;`"></div>
+                </div>
+                <div class="px-2 py-1 x text-xs justify-between">
+                    <span>{{ goal.patron_count / 20 * 100 }}%</span>
+                    <span>{{ goal.patron_count }}/{{ patronsGoal }} patrons/mo</span>
+                </div>
             </div>
-        </div>
-        <p class="px-2 py-1 transition-colors x bg-white dark:bg-ui-900" :class="{'text-red-500': !goal}">{{ goal.description }}</p>
+        </template>
+        <p v-else class="px-2 py-1 transition-colors x bg-white dark:bg-ui-900 text-red-500">{{ goal.error }}</p>
     </div>
 </template>
--- resources/js/stores/statistics.ts
+++ resources/js/stores/statistics.ts
@@ -4,13 +4,10 @@ import {usePage} from "@inertiajs/vue3";
 
 export const useStatisticsStore = defineStore('statistics', () => {
     const counts = ref(usePage<StatisticsProps>().props.app.statistics.counts);
-    const patreon = ref(usePage<StatisticsProps>().props.app.statistics.patreon && usePage<StatisticsProps>().props.app.statistics.patreon.included ? usePage<StatisticsProps>().props.app.statistics.patreon.included[0].attributes : {
-        completed_percentage: NaN,
-        amount_cents: NaN,
-        description: 'Could Not Connect to Patreon API'
+    const patreon = ref(usePage<StatisticsProps>().props.app.statistics.patreon ? usePage<StatisticsProps>().props.app.statistics.patreon.data.attributes : {
+        patron_count: NaN,
+        error: 'Could Not Connect to Patreon API'
     });
-
-
 
     return {counts, patreon};
 })
