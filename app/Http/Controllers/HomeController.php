eae0f69b20dd0fa66d534b0e1b97b4ece0f624e3
0x1DEA <seebeyondedm@gmail.com>
2023-06-15 01:32:54 -0500

    Add featured and magic to levels page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -24,6 +26,25 @@ class HomeController extends Controller
 
     public function levels(): Response
     {
-        return Inertia::render('Levels');
+        $featured = Cache::get('levels.featured');
+        if (!$featured) {
+            $featured = Http::get('https://gdbrowser.com/api/search/gdforums?type=featured')->json();
+            Cache::put(json_encode($featured), 3600);
+        } else {
+            $featured = json_decode($featured);
+        }
+
+        $magic = Cache::get('levels.magic');
+        if (!$magic) {
+            $magic = Http::get('https://gdbrowser.com/api/search/gdforums?type=magic')->json();
+            Cache::put(json_encode($magic), 3600);
+        } else {
+            $magic = json_decode($magic);
+        }
+
+        return Inertia::render('Levels', [
+            'featured' => $featured,
+            'magic' => $magic
+        ]);
     }
 }
--- resources/js/Pages/Levels.vue
+++ resources/js/Pages/Levels.vue
@@ -3,6 +3,13 @@ import AppLayout from '@/Layouts/Dash.vue'
 import { Link } from '@inertiajs/vue3';
 import StatsPanel from "@/Components/StatsPanel.vue";
 import DiscordInvite from "@/Components/DiscordInvite.vue";
+import Carousel from "@/Components/Carousel.vue";
+import route from "ziggy-js";
+
+const props = defineProps({
+    featured: Object,
+    magic: Object
+})
 </script>
 <template>
     <app-layout title="Home">
@@ -15,6 +22,26 @@ import DiscordInvite from "@/Components/DiscordInvite.vue";
                 <div class="pane">
                     <Link class="underline" :href="route('levels.index')">Click here to visit the level reviews portal</Link>
                 </div>
+                <h2 class="mx-2 font-bold text-xl">Featured</h2>
+                <Carousel decorations="px-10">
+                    <Link :href="route('levels.show', level.id)" class="x items-center bg-ui-800 rounded-lg px-4 py-2 shrink-0 min-w-[16rem]" v-for="level in featured">
+                        <img alt="Difficulty" class="h-24 mr-4" :src="`https://browser.gdps.io/assets/difficulties/${level.difficultyFace}.png`"/>
+                        <div class="y">
+                            <h2 class="text-2xl font-bold">{{ level.name }}</h2>
+                            <span class="text-sm">{{ level.author }}</span>
+                        </div>
+                    </Link>
+                </Carousel>
+                <h2 class="mx-2 font-bold text-xl">Mod Queue</h2>
+                <Carousel decorations="px-10">
+                    <Link :href="route('levels.show', level.id)" class="x items-center bg-ui-800 rounded-lg px-4 py-2 shrink-0 min-w-[16rem]" v-for="level in magic">
+                        <img alt="Difficulty" class="h-24 mr-4" :src="`https://browser.gdps.io/assets/difficulties/${level.difficultyFace}.png`"/>
+                        <div class="y">
+                            <h2 class="text-2xl font-bold">{{ level.name }}</h2>
+                            <span class="text-sm">{{ level.author }}</span>
+                        </div>
+                    </Link>
+                </Carousel>
             </div>
         </div>
         <div class="y space-y-2 md:w-1/4">
