6a12f07b7f57f2dfb3ad3a3d54c5abf61c2cab2d
0x1DEA <seebeyondedm@gmail.com>
2023-06-21 01:07:23 -0500

    User card tooltip

--- resources/js/Components/Username.vue
+++ resources/js/Components/Username.vue
@@ -1,6 +1,8 @@
 <script setup>
 import route from 'ziggy-js'
 import {Link} from '@inertiajs/vue3';
+import Tooltip from "@/Components/Tooltip.vue";
+import Avatar from "@/Components/Avatar.vue";
 
 const props = defineProps({
     user: Object,
@@ -12,5 +14,20 @@ const props = defineProps({
 })
 </script>
 <template>
-    <Link :href="href ?? route('users.show', user.id)" class="transition-colors text-ui-200 break-all">{{ user.name }}</Link>
+    <Tooltip :caret="false" :decoration="false" container-class="pb-1 bottom-full">
+        <Link :href="href ?? route('users.show', user.id)" class="transition-colors text-ui-200 break-all">{{ user.name }}</Link>
+        <template #content>
+            <div class="y box shadow-xl !p-0 overflow-hidden">
+                <div class="bg-ui-600 py-12 bg-cover bg-center" :style="`background-image: url('${user.banner_url}');`"></div>
+                <div class="x space-x-2 bg-ui-800 px-2 min-w-[20rem]">
+                    <Avatar class="h-20 -mt-12 mb-2" :user="user"/>
+                    <div class="y">
+                        <div class="x space-x-2">
+                            <span class="font-bold text-lg">{{ user.name }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </Tooltip>
 </template>
