78765d0b30971c03db3b643af7b3676561f3de1e
0x1DEA <seebeyondedm@gmail.com>
2023-06-21 01:06:53 -0500

    Add more tooltip options

--- resources/js/Components/Tooltip.vue
+++ resources/js/Components/Tooltip.vue
@@ -4,6 +4,15 @@ const props = defineProps({
     caret: {
         type: Boolean,
         default: true
+    },
+    decoration: {
+        type: Boolean,
+        default: true
+    },
+    // TODO: Better positioning
+    containerClass: {
+        type: String,
+        default: ''
     }
 })
 
@@ -14,18 +23,19 @@ const props = defineProps({
             <slot/>
         </div>
 
-        <div v-if="$slots.content || message" class="absolute left-0 transition-[visibility] duration-0 invisible group-hover:visible">
+        <div v-if="$slots.content || message" class="z-10 absolute left-0 transition-[visibility] duration-0 invisible group-hover:visible" :class="`${containerClass}`">
             <div class="y items-center w-full pt-2 transition ease-out transform duration-75 opacity-0 scale-95 group-hover:opacity-100 group-hover:duration-200 group-hover:scale-100">
                 <template v-if="caret">
                     <div class="drop-shadow absolute border-[8px] border-t-0 top-0 border-transparent border-b-ui-700"></div>
                     <div class="z-10 absolute border-[8px] border-t-0 top-[1.5px] border-transparent border-b-ui-900"></div>
                 </template>
-                <div class="shadow-lg box !py-0 !px-0">
+                <div v-if="decoration" class="shadow-lg box !py-0 !px-0">
                     <div v-if="message" class="px-2">
                         <span class="text-center text-xs select-none whitespace-nowrap">{{ message }}</span>
                     </div>
                     <slot v-else name="content"/>
                 </div>
+                <slot v-else name="content"/>
             </div>
         </div>
     </div>
