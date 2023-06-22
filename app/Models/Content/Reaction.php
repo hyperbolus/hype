d5dd115ac741f382b87177606e0ab3bf3bab74f9
0x1DEA <seebeyondedm@gmail.com>
2023-06-22 00:07:53 -0500

    Update reacter selects for user card

--- app/Models/Content/Reaction.php
+++ app/Models/Content/Reaction.php
@@ -17,7 +17,7 @@ class Reaction extends Model
 
     public function reacter(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'reacter_id')->select(['id', 'name']);
+        return $this->belongsTo(User::class, 'reacter_id')->select(['id', 'name', 'created_at', 'last_seen', 'time_online', 'pronouns', 'avatar_url', 'banner_url']);
     }
 
     public function reactable(): MorphTo
