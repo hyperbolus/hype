41d02dcefd2752084dc858d39d90717721b9e29e
0x1DEA <seebeyondedm@gmail.com>
2023-06-21 23:12:12 -0500

    Auth utility functions

--- resources/js/util.js
+++ resources/js/util.js
@@ -1,6 +1,6 @@
 import {useDark, useToggle} from "@vueuse/core";
 import {ref} from "vue";
-import {router} from "@inertiajs/vue3";
+import {router, usePage} from "@inertiajs/vue3";
 import route from "ziggy-js";
 import {useSettingsStore} from "@/stores/settings.ts";
 
@@ -19,6 +19,14 @@ export const toggleDark = () => {
 
 export const logout = () => {
     router.post(route('auth::logout'));
+}
+
+export const isAdmin = () => {
+    return isAuthenticated() && usePage().props.user.roles.includes('admin')
+}
+
+export const isAuthenticated = () => {
+    return usePage().props.auth;
 }
 
 export const invisiblePixel = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII=';
