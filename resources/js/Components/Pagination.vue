5b3860c7d1b96f8d4b1100924395d00312854c3c
0x1DEA <seebeyondedm@gmail.com>
2023-06-22 15:11:19 -0500

    Make jump to page on pagination a bit more obvious + hover indicator

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -17,30 +17,30 @@ const promptPage = () => {
     <div class="x justify-center">
         <div class="x items-center pane overflow-clip !py-0 !px-0 !rounded-md w-fit">
             <template v-if="list.current_page !== 1">
-                <Link v-if="list.current_page - 1 !== 1" :href="list.first_page_url" class="px-1">
+                <Link v-if="list.current_page - 1 !== 1" :href="list.first_page_url" class="px-1 hover:text-ui-500">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                         <path fill-rule="evenodd" d="M15.79 14.77a.75.75 0 01-1.06.02l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 111.04 1.08L11.832 10l3.938 3.71a.75.75 0 01.02 1.06zm-6 0a.75.75 0 01-1.06.02l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 111.04 1.08L5.832 10l3.938 3.71a.75.75 0 01.02 1.06z" clip-rule="evenodd" />
                     </svg>
                 </Link>
-                <Link :href="list.prev_page_url" class="px-1">
+                <Link :href="list.prev_page_url" class="px-1 hover:text-ui-500">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                         <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
                     </svg>
                 </Link>
             </template>
             <template v-for="(link, index) in list.links.slice(1, -1)">
-                <Link v-if="link.label !== '...'" :href="link.url" :key="index" class="px-2 bg-opacity-50 transition-colors" :class="{'bg-ui-700': link.active}">
+                <Link v-if="link.label !== '...'" :href="link.url" :key="index" class="px-2 transition-colors hover:text-ui-500" :class="{'bg-ui-700': link.active}">
                     {{ link.label }}
                 </Link>
-                <span @click="promptPage" v-else>{{ link.label }}</span>
+                <span @click="promptPage" v-else class="cursor-pointer hover:text-ui-500" title="Jump to specific page">{{ link.label }}</span>
             </template>
             <template v-if="list.current_page !== list.last_page">
-                <Link :href="list.next_page_url" class="px-1">
+                <Link :href="list.next_page_url" class="px-1 hover:text-ui-500">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                         <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
                     </svg>
                 </Link>
-                <Link v-if="list.current_page + 1 !== list.links.length - 2" :href="list.last_page_url" class="px-1">
+                <Link v-if="list.current_page + 1 !== list.links.length - 2" :href="list.last_page_url" class="px-1 hover:text-ui-500">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                         <path fill-rule="evenodd" d="M10.21 14.77a.75.75 0 01.02-1.06L14.168 10 10.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
                         <path fill-rule="evenodd" d="M4.21 14.77a.75.75 0 01.02-1.06L8.168 10 4.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
