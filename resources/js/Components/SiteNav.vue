f42bc1064172bdb0ffc6a29719b962035b955951
0x1DEA <seebeyondedm@gmail.com>
2023-06-22 17:12:32 -0500

    Redesign desktop navbar

--- resources/js/Components/SiteNav.vue
+++ resources/js/Components/SiteNav.vue
@@ -16,14 +16,17 @@ const mobileNavOpen = ref(false);
 const navigation = useSettingsStore().settings['navigation'] ? useSettingsStore().settings['navigation']['value'] : [];
 </script>
 <template>
-    <div class="y top-0 items-center relative z-20 transition-colors w-full py-4 bg-ui-900">
+    <div class="y group/nav items-center relative transition-colors w-full py-4 bg-ui-900">
+        <div class="x z-10 transition-opacity opacity-0 group-hover/nav:opacity-100 absolute w-full h-48 pointer-events-none top-full bg-gradient-to-b from-ui-900 to-transparent">
+
+        </div>
         <div class="x px-2 gap-4 justify-between lg:max-w-5xl xl:max-w-6xl w-full">
             <div class="x items-center space-x-4">
                 <SiteLogo/>
                 <div class="hidden md:flex items-center space-x-4 text-sm">
                     <span v-if="navigation.length === 0" class="text-red-500">No key 'navigation' defined in site settings</span>
                     <template v-for="(node, key) in navigation">
-                        <Tooltip v-if="node.hasOwnProperty('children')" :caret="false">
+                        <Tooltip v-if="node.hasOwnProperty('children')" :caret="false" :decoration="false">
                             <div class="x items-center">
                                 <Link :href="route(node.route)" class="hover:text-ui-500 transition transition-colors">{{ node.name }}</Link>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
@@ -31,8 +34,8 @@ const navigation = useSettingsStore().settings['navigation'] ? useSettingsStore(
                                 </svg>
                             </div>
                             <template #content>
-                                <div class="y divide-y divide-ui-700 w-32">
-                                    <Link v-for="(child, key) in node.children" :key="key" class="px-2 py-1 hover:bg-ui-800" :href="route(child.route)">{{ child.name }}</Link>
+                                <div class="y -divide-y -divide-ui-700 min-w-[8rem] -ml-2">
+                                    <Link v-for="(child, key) in node.children" :key="key" class="px-2 py-1 hover:bg-ui-800 rounded" :href="route(child.route)"><span class="bg-green-400 rounded-full px-[0.15rem] mr-2"></span> {{ child.name }}</Link>
                                 </div>
                             </template>
                         </Tooltip>
@@ -40,13 +43,7 @@ const navigation = useSettingsStore().settings['navigation'] ? useSettingsStore(
                     </template>
                 </div>
             </div>
-            <div class="hidden md:flex items-center">
-                <ControlBar v-if="$page.props.auth"/>
-                <div v-else class="x items-center space-x-4">
-                    <Link :href="route('auth::login')">Login</Link>
-                    <Link :href="route('auth::register')" class="bg-cyan-500 bg-ui-700 text-ui-200 rounded-md font-bold px-2 py-0.5">Register</Link>
-                </div>
-            </div>
+            <ControlBar class="hidden md:flex"/>
             <div class="x md:hidden items-center cursor-pointer">
                 <svg @click="mobileNavOpen = !mobileNavOpen" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
