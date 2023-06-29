c5bad5346e538ad7a0d2cd40e298bb1b871a0a68
0x1DEA <seebeyondedm@gmail.com>
2023-06-28 23:55:05 -0500

    Wrong title for Tags page

--- resources/js/Pages/Levels/Tags.vue
+++ resources/js/Pages/Levels/Tags.vue
@@ -91,7 +91,7 @@ onMounted(() => {
 })
 </script>
 <template>
-    <app-layout title="Home">
+    <app-layout title="Tags">
         <template #breadcrumbs>
             <Link :href="route('levels.show', level.id)">{{ level.id }}</Link>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
