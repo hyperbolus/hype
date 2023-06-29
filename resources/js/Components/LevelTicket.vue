c30f5cccac2ebd3d8232b1759f29f3b0cb928fdc
0x1DEA <seebeyondedm@gmail.com>
2023-06-29 17:23:18 -0500

    reorder and clarify scores

--- resources/js/Components/LevelTicket.vue
+++ resources/js/Components/LevelTicket.vue
@@ -40,6 +40,7 @@ const displayRating = (rating) => {
     return rating ? rating.toFixed(1) : '-';
 }
 // TODO: Fucking disgusting, clean it up
+// border-2 border-red-50 rounded-lg shadow-[0_0_5px_3px_rgba(240,240,100,1)]
 </script>
 <template>
     <Link :href="route('levels.show', level.id)">
@@ -74,26 +75,26 @@ const displayRating = (rating) => {
                             </div>
                         </div>
                         <div class="x z-10 md:-translate-x-8 justify-center sm:w-fit sm:skew-x-12 sm:rounded-tr-lg bg-ui-800">
-                            <div class="grid sm:-skew-x-12 grid-cols-4 sm:grid-cols-5 md:ml-8 gap-4 py-1.5 px-4 transition-[text-shadow] [text-shadow:black_0_0_10px]">
-                                <span class="y">
-                                    <span class="text-xs uppercase">Difficulty</span>
-                                    <span class="text-lg font-bold">{{ displayRating(level.rating_difficulty) }}</span>
-                                </span>
-                                <span class="y">
-                                    <span class="text-xs uppercase">Gameplay</span>
-                                    <span class="text-lg font-bold">{{ displayRating(level.rating_gameplay) }}</span>
-                                </span>
-                                <span class="y">
-                                    <span class="text-xs uppercase">Visuals</span>
-                                    <span class="text-lg font-bold">{{ displayRating(level.rating_visuals) }}</span>
-                                </span>
-                                <span class="y">
-                                    <span class="text-xs uppercase">Overall</span>
-                                    <span class="text-lg font-bold">{{ displayRating(level.rating_overall) }}</span>
-                                </span>
+                            <div class="grid items-end sm:-skew-x-12 grid-cols-4 sm:grid-cols-5 md:ml-8 gap-4 py-1.5 px-4 -[text-shadow:black_0_0_10px]">
                                 <span class="hidden sm:flex flex-col">
                                     <span class="text-xs uppercase">Reviews</span>
                                     <span class="text-lg font-bold">{{ level.reviews_count }}</span>
+                                </span>
+                                <span class="y">
+                                    <span class="text-xs uppercase">Difficulty</span>
+                                    <span class="text-lg font-bold">{{ displayRating(level.rating_difficulty) }}<span class="text-xs text-ui-600">/100</span></span>
+                                </span>
+                                <span class="y">
+                                    <span class="text-xs uppercase">Overall</span>
+                                    <span class="text-lg font-bold">{{ displayRating(level.rating_overall) }}<span class="text-xs text-ui-600">/10</span></span>
+                                </span>
+                                <span class="y">
+                                    <span class="text-xs uppercase">Gameplay</span>
+                                    <span class="text-lg font-bold">{{ displayRating(level.rating_gameplay) }}<span class="text-xs text-ui-600">/10</span></span>
+                                </span>
+                                <span class="y">
+                                    <span class="text-xs uppercase">Visuals</span>
+                                    <span class="text-lg font-bold">{{ displayRating(level.rating_visuals) }}<span class="text-xs text-ui-600">/10</span></span>
                                 </span>
                             </div>
                         </div>
