6df1d0b99b7bb07c06ba35118de74dd56fe0ba44
0x1DEA <seebeyondedm@gmail.com>
2023-07-27 22:41:52 -0500

    better center and color tiny text

--- resources/js/Components/ReportModal.vue
+++ resources/js/Components/ReportModal.vue
@@ -62,11 +62,11 @@ onMounted(() => {
             <label>
                 Details
                 <textarea v-model="report.details" placeholder="Required" class="textbox !bg-ui-950"></textarea>
-                <span class="x items-center text-ui-600 text-sm mt-0.5">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 mr-0.5">
-                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clip-rule="evenodd" />
-                </svg>
-                Please be as specific as possible
+                <span class="x items-center text-ui-500 text-sm mt-0.5">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 mr-0.5">
+                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clip-rule="evenodd" />
+                    </svg>
+                    <span>Please be as specific as possible</span>
                 </span>
             </label>
             <ul v-if="Object.keys($page.props.errors).length > 0" class="mt-3 list-disc list-inside text-sm text-red-600">
