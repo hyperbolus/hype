5c598154c0bab0c37d619271162b7dc16d407f3a
0x1DEA <seebeyondedm@gmail.com>
2023-09-19 01:49:58 -0500

    Macro fps, coin mask, and mod rating

---
+++ database/migrations/tenant/0.3.0/2023_09_17_193257_extend_level_replays.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('level_replays', function (Blueprint $table) {
+            $table->unsignedTinyInteger('coins_collected')->nullable()->after('coins');
+            $table->decimal('fps');
+            $table->tinyInteger('rating');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('level_replays', function (Blueprint $table) {
+            $table->dropColumn('coins_collected');
+            $table->dropColumn('fps');
+            $table->dropColumn('rating');
+        });
+    }
+};
