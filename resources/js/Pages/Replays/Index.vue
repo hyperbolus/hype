a8d307a7ed76aa2030e7fe758a50e1cc44b9d20f
0x1DEA <seebeyondedm@gmail.com>
2023-09-21 11:45:52 -0500

    Remove macro upload filter

--- resources/js/Pages/Replays/Index.vue
+++ resources/js/Pages/Replays/Index.vue
@@ -200,7 +200,7 @@ const search = () => {
 
                         <Label ref="dropzone">
                             Macro File
-                            <input @change="updateFile" class="hidden" type="file" accept=".replay,.zbot,.dat,.ddhor,.xbot,.kd,.zbf,.xbot,.rsh,.json,.mhr"/>
+                            <input @change="updateFile" class="hidden" type="file"/>
                             <div v-if="submission.file === null" class="y items-center rounded-md cursor-pointer bg-ui-950 border-2 border-dashed border-ui-700 p-2">
                                 <svg :class="{'text-green-500': isOverDropZone}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
