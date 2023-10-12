bfa13a8d8b966877dcf97e9ea40c246eb62306fd
0x1DEA <seebeyondedm@gmail.com>
2023-10-11 21:36:12 -0500

    Add option to make avatar a link

--- resources/js/Components/Avatar.vue
+++ resources/js/Components/Avatar.vue
@@ -1,12 +1,16 @@
 <script setup>
+import {Link} from "@inertiajs/vue3";
+import route from "ziggy-js";
+
 const props = defineProps({
     user: Object,
-    width: String
+    width: String,
+    link: Boolean
 })
 </script>
 <template>
-    <span class="relative">
+    <component :is="link ? Link : 'span'" :href="link ? route('users.show', user.id) : null" class="relative">
         <span class="absolute z-0 rounded-full bg-ui-700 animate-pulse inset-0"></span>
         <img class="relative z-10 rounded-full" :src="user.avatar_url ?? '/api/avatar?name=' + user.name" alt="avatar" :class="width ?? 'w-8'"/>
-    </span>
+    </component>
 </template>
