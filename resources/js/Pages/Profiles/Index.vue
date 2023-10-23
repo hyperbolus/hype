7c1e2f557813027fd4008a9f5c6e15d38f79bed2
0x1DEA <seebeyondedm@gmail.com>
2023-10-22 22:40:42 -0500

    Basic profile index

---
+++ resources/js/Pages/Profiles/Index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppLayout from "@/Layouts/Dash.vue";
+import Pagination from "@/Components/Pagination.vue";
+
+const props = defineProps({
+    profiles: Object
+})
+</script>
+<template>
+    <app-layout title="Profiles Index">
+        <div class="y space-y-2 w-full">
+            <Pagination :list="profiles"/>
+            <div class="pane" v-for="profile in profiles.data">{{ profile.username }}</div>
+            <Pagination :list="profiles"/>
+        </div>
+    </app-layout>
+</template>
