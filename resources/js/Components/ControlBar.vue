8592274dfd1bea0bd5004af55e4cd1132882a0e9
0x1DEA <seebeyondedm@gmail.com>
2023-12-27 00:53:05 -0600

    Better search bar

--- resources/js/Components/ControlBar.vue
+++ resources/js/Components/ControlBar.vue
@@ -8,13 +8,14 @@ import {logout, isDark, toggleDark} from '@/util.js'
 import {ref} from "vue";
 import {useSettingsStore} from "@/stores/settings.ts";
 import SearchBar from "@/Components/SearchBar.vue";
+import Icon from "@/Components/Icon.vue";
 
 const mobileNavOpen = ref(false);
 const navigation = useSettingsStore().settings['navigation'] ? useSettingsStore().settings['navigation']['value'] : [];
 </script>
 <template>
+    <SearchBar class="mr-4"/>
     <div v-if="$page.props.auth" class="flex items-center space-x-4">
-        <SearchBar/>
         <Dropdown container-classes="min-w-[20rem]">
             <template #trigger>
                 <div class="relative">
@@ -22,9 +23,7 @@ const navigation = useSettingsStore().settings['navigation'] ? useSettingsStore(
                         <span class="animate-ping absolute inline-flex bg-blue-500 rounded-full h-full w-full opacity-75"></span>
                         <span class="inline-flex bg-blue-500 rounded-full h-3 w-3"></span>
                     </span>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                        <path fill-rule="evenodd" d="M10 2a6 6 0 00-6 6c0 1.887-.454 3.665-1.257 5.234a.75.75 0 00.515 1.076 32.91 32.91 0 003.256.508 3.5 3.5 0 006.972 0 32.903 32.903 0 003.256-.508.75.75 0 00.515-1.076A11.448 11.448 0 0116 8a6 6 0 00-6-6zM8.05 14.943a33.54 33.54 0 003.9 0 2 2 0 01-3.9 0z" clip-rule="evenodd" />
-                    </svg>
+                    <Icon class="w-5" size="24" name="bell"/>
                 </div>
             </template>
             <template #content>
@@ -49,23 +48,17 @@ const navigation = useSettingsStore().settings['navigation'] ? useSettingsStore(
                 <span class="animate-ping absolute inline-flex bg-blue-500 rounded-full h-full w-full opacity-75"></span>
                 <span class="inline-flex bg-blue-500 rounded-full h-3 w-3"></span>
             </span>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                <path fill-rule="evenodd" d="M2 10c0-3.967 3.69-7 8-7 4.31 0 8 3.033 8 7s-3.69 7-8 7a9.165 9.165 0 01-1.504-.123 5.976 5.976 0 01-3.935 1.107.75.75 0 01-.584-1.143 3.478 3.478 0 00.522-1.756C2.979 13.825 2 12.025 2 10z" clip-rule="evenodd" />
-            </svg>
+            <Icon class="w-5" size="24" name="chat-bubble-oval-left"/>
         </Link>
         <Link v-if="$page.props.user.roles.includes('admin')" :href="route('reports.index')">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
-                <path fill-rule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
-            </svg>
+            <Icon class="w-5" size="24" name="exclamation-triangle"/>
         </Link>
         <Dropdown class="shrink-0">
             <template #trigger>
                 <div class="x bg-ui-950 p-1 rounded-full items-center cursor-pointer">
                     <Avatar class="w-8 mr-2" :user="$page.props.user"/>
                     <span>{{ $page.props.user.name }}</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="shrink-0 mx-1 w-5 h-5">
-                        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                    </svg>
+                    <Icon class="w-5 mx-1" size="20" name="chevron-down"/>
                 </div>
             </template>
             <template #content>
--- resources/js/Components/SearchBar.vue
+++ resources/js/Components/SearchBar.vue
@@ -1,12 +1,12 @@
 <script setup>
-import {ref, watch} from 'vue'
+import {onUnmounted, ref, watch} from 'vue'
 import { MeiliSearch } from 'meilisearch'
 import { Link } from '@inertiajs/vue3'
 import route from 'ziggy-js'
 import {face} from "@/util.js";
 import Username from "@/Components/Username.vue";
-
-const focused = ref(true);
+import Icon from "@/Components/Icon.vue";
+import Dropdown from "@/Jetstream/Dropdown.vue";
 
 const client = new MeiliSearch({
     host: 'https://search.gdps.io',
@@ -39,64 +39,59 @@ watch(newQuery, async () => {
     lastQuery.value = newQuery.value;
 })
 
-const focusEvent = (e) => {
-    if (!document.getElementById('globalSearchBar').contains(e.target)) {
-        focused.value = false;
-        document.removeEventListener('mousedown', focusEvent)
-    }
-};
-const handleFocus = () => {
-    focused.value = true;
-    document.addEventListener('mousedown', focusEvent);
-}
-
-/**
- * @type {Intl.NumberFormat}
- */
+/** @type {Intl.NumberFormat} */
 const format = Intl.NumberFormat('en-US', {
     notation: "compact",
     maximumFractionDigits: 1
 });
 
-const tiny = (n) => {
-    return format.format(n);
-}
+const tiny = n => format.format(n);
+
 </script>
 <template>
-    <div id="globalSearchBar" @focusin="handleFocus" class="x h-fit relative items-center rounded-lg bg-ui-800">
-        <input placeholder="Search..." v-model="newQuery" type="text" class="pl-3 w-full truncate shrink text-sm py-1.5 border-none focus-visible:ring-0 bg-transparent placeholder-ui-500"/>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="shrink-0 w-4 h-4 mr-3">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-        </svg>
-        <!-- Changing this to v-show breaks because lastQuery (null) is undefined on searches cache -->
-        <div v-if="focused && searches[lastQuery]" class="absolute border border-ui-700 bg-ui-900 rounded-md py-1 space-y-2 shadow-xl top-12 z-30 min-w-full">
-            <div class="px-2" v-if="searches[lastQuery].results.map(r => r.totalHits).reduce((p, a) => p + a, 0) === 0">No results</div>
-            <div class="px-2 text-sm text-ui-500" v-else>{{ searches[lastQuery].results.map(r => r.totalHits).reduce((p, a) => p + a, 0) }} results in {{ Math.max(...searches[lastQuery].results.map(r => r.processingTimeMs)) }}ms</div>
-            <div class="y px-2 !mt-0 py-1 space-y-1" v-if="searches[lastQuery].results[0].hits.length > 0">
-                <span class="text-xs text-ui-500 font-bold uppercase">Levels</span>
-                <Link v-for="hit in searches[lastQuery].results[0].hits" :href="route('levels.show', hit.id)" class="x space-x-2 items-center">
-                    <img class="w-10" alt="Difficulty" :src="face(hit)"/>
-                    <div class="y leading-tight">
-                        <div>{{ hit.name }}</div>
-                        <div class="text-xs text-ui-500">{{ hit.creator }}</div>
-                        <div class="text-xs text-ui-400 x items-center space-x-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 h-3">
-                                <path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
-                                <path fill-rule="evenodd" d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                            </svg>
-                            <div class="mr-4">{{ tiny(hit.downloads) }}</div>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 h-3" :class="{'rotate-180': hit.likes < 0}">
-                                <path d="M1 8.25a1.25 1.25 0 112.5 0v7.5a1.25 1.25 0 11-2.5 0v-7.5zM11 3V1.7c0-.268.14-.526.395-.607A2 2 0 0114 3c0 .995-.182 1.948-.514 2.826-.204.54.166 1.174.744 1.174h2.52c1.243 0 2.261 1.01 2.146 2.247a23.864 23.864 0 01-1.341 5.974C17.153 16.323 16.072 17 14.9 17h-3.192a3 3 0 01-1.341-.317l-2.734-1.366A3 3 0 006.292 15H5V8h.963c.685 0 1.258-.483 1.612-1.068a4.011 4.011 0 012.166-1.73c.432-.143.853-.386 1.011-.814.16-.432.248-.9.248-1.388z" />
-                            </svg>
-                            <div>{{ tiny(hit.likes) }}</div>
-                        </div>
+    <Dropdown id="globalSearchBar" content-classes="w-fit">
+        <template #trigger>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-5 h-5">
+                <path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" />
+            </svg>
+
+        </template>
+        <template #content>
+            <div @click.stop class="y items-stretch rounded-md space-y-2 shadow-xl z-30 w-64">
+                <div class="flex items-center bg-ui-800 px-2 rounded-t-md">
+                    <Icon class="w-5" size="24" type="solid" name="magnifying-glass"/>
+                    <input placeholder="Search..." v-model="newQuery" type="text" class="pl-3 w-full truncate shrink text-sm py-1.5 border-none focus-visible:ring-0 bg-transparent placeholder-ui-500"/>
+                </div>
+                <div class="y space-y-2 w-full" v-if="searches[lastQuery]">
+                    <div class="px-2" v-if="searches[lastQuery].results.map(r => r.totalHits).reduce((p, a) => p + a, 0) === 0">No results</div>
+                    <div class="px-2 text-sm text-ui-500" v-else>{{ searches[lastQuery].results.map(r => r.totalHits).reduce((p, a) => p + a, 0) }} results in {{ Math.max(...searches[lastQuery].results.map(r => r.processingTimeMs)) }}ms</div>
+                    <div class="y px-2 !mt-0 py-1 space-y-1" v-if="searches[lastQuery].results[0].hits.length > 0">
+                        <span class="text-xs text-ui-500 font-bold uppercase">Levels</span>
+                        <Link v-for="hit in searches[lastQuery].results[0].hits" :href="route('levels.show', hit.id)" class="x space-x-2 items-center">
+                            <img class="w-10" alt="Difficulty" :src="face(hit)"/>
+                            <div class="y leading-tight">
+                                <div>{{ hit.name }}</div>
+                                <div class="text-xs text-ui-500">{{ hit.creator }}</div>
+                                <div class="text-xs text-ui-400 x items-center space-x-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 h-3">
+                                        <path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
+                                        <path fill-rule="evenodd" d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
+                                    </svg>
+                                    <div class="mr-4">{{ tiny(hit.downloads) }}</div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 h-3" :class="{'rotate-180': hit.likes < 0}">
+                                        <path d="M1 8.25a1.25 1.25 0 112.5 0v7.5a1.25 1.25 0 11-2.5 0v-7.5zM11 3V1.7c0-.268.14-.526.395-.607A2 2 0 0114 3c0 .995-.182 1.948-.514 2.826-.204.54.166 1.174.744 1.174h2.52c1.243 0 2.261 1.01 2.146 2.247a23.864 23.864 0 01-1.341 5.974C17.153 16.323 16.072 17 14.9 17h-3.192a3 3 0 01-1.341-.317l-2.734-1.366A3 3 0 006.292 15H5V8h.963c.685 0 1.258-.483 1.612-1.068a4.011 4.011 0 012.166-1.73c.432-.143.853-.386 1.011-.814.16-.432.248-.9.248-1.388z" />
+                                    </svg>
+                                    <div>{{ tiny(hit.likes) }}</div>
+                                </div>
+                            </div>
+                        </Link>
                     </div>
-                </Link>
+                    <div class="y px-2" v-if="searches[lastQuery].results[1].hits.length > 0">
+                        <span class="text-xs text-ui-500 font-bold uppercase">Users</span>
+                        <Username v-for="hit in searches[lastQuery].results[1].hits" :user="hit"/>
+                    </div>
+                </div>
             </div>
-            <div class="y px-2" v-if="searches[lastQuery].results[1].hits.length > 0">
-                <span class="text-xs text-ui-500 font-bold uppercase">Users</span>
-                <Username v-for="hit in searches[lastQuery].results[1].hits" :user="hit"/>
-            </div>
-        </div>
-    </div>
+        </template>
+    </Dropdown>
 </template>
