ffe2a173c2a465996403526ecbcb8059cd87a991
0x1DEA <seebeyondedm@gmail.com>
2024-06-17 15:07:08 -0500

    maybe we didnt need those double arrows

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -13,12 +13,13 @@ const promptPage = () => {
     let url = first.substring(0, first.length - 1) + page;
     router.get(url);
 }
+// TODO: logic for showing insta hop. maybe remove or use elipses to determine if they should show?
 </script>
 <template>
     <div class="x justify-center">
         <div class="x items-center pane overflow-hidden !py-0 !px-0 !rounded-md w-fit">
             <template v-if="list.current_page !== 1">
-                <Link v-if="list.current_page - 1 !== 1" :href="list.first_page_url" class="hidden sm:flex px-1 hover:text-ui-500">
+                <Link v-if="false && list.current_page - 1 !== 1" :href="list.first_page_url" class="hidden sm:flex px-1 hover:text-ui-500">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                         <path fill-rule="evenodd" d="M15.79 14.77a.75.75 0 01-1.06.02l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 111.04 1.08L11.832 10l3.938 3.71a.75.75 0 01.02 1.06zm-6 0a.75.75 0 01-1.06.02l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 111.04 1.08L5.832 10l3.938 3.71a.75.75 0 01.02 1.06z" clip-rule="evenodd" />
                     </svg>
@@ -46,7 +47,7 @@ const promptPage = () => {
                         <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
                     </svg>
                 </Link>
-                <Link v-if="list.current_page + 1 !== list.links.length - 2" :href="list.last_page_url" class="hidden sm:flex px-1 hover:text-ui-500">
+                <Link v-if="false && list.current_page + 1 !== list.links.length - 2" :href="list.last_page_url" class="hidden sm:flex px-1 hover:text-ui-500">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                         <path fill-rule="evenodd" d="M10.21 14.77a.75.75 0 01.02-1.06L14.168 10 10.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
                         <path fill-rule="evenodd" d="M4.21 14.77a.75.75 0 01.02-1.06L8.168 10 4.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
