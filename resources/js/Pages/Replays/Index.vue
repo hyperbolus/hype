0465f0cc81eb6973673964ac7969bfef52f924b1
0x1DEA <seebeyondedm@gmail.com>
2024-06-17 21:57:44 -0500

    remove icreate format banner and add check in case macro has no assoc. files

--- resources/js/Pages/Replays/Index.vue
+++ resources/js/Pages/Replays/Index.vue
@@ -178,12 +178,6 @@ const search = () => {
                 </svg>
                 <span>If you have an academic interest in the full dataset or would like to contribute to our research, please <Tooltip message="Discord Invite" :inline="true"><a class="text-white underline" href="https://discord.gg/JJc8nndmFE">contact us!</a></Tooltip></span>
             </div>
-            <div class="x space-x-2 items-center rounded-md p-2 bg-green-500/10 border border-green-500">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="shrink-0 text-green-500 w-8 h-8">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
-                </svg>
-                <span>For iCreate users, your format is .zbf or "zBot Frame," the same as zBot.</span>
-            </div>
             <Link :href="route('replays.create')" :disabled="!isAuthenticated()" class="font-bold w-full rounded-md px-2 py-1 shadow bg-ui-800 border-0" :class="isAuthenticated() ? 'hover:bg-ui-900 hover:shadow-sm' : 'cursor-not-allowed'">{{ isAuthenticated() ? 'Submit' : 'Log in to submit' }} a record</Link>
             <h2 class="font-bold text-xl">Statistics</h2>
             <div class="grid grid-cols-2 lg:grid-cols-4">
@@ -262,7 +256,7 @@ const search = () => {
                             </div>
                             <div class="y items-end">
                                 <span>{{ macro.format }} | FPS: {{ macro.fps ?? 'Unknown' }}</span>
-                                <Tooltip :message="macro.files[0].filename">
+                                <Tooltip v-if="macro.files.length > 0" :message="macro.files[0].filename">
                                     <a :download="macro.files[0].filename" :href="macro.files[0].url"><span class="text-sm underline text-white">Download</span></a>
                                 </Tooltip>
                             </div>
