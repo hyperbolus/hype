c70dbd208c1c4cebc1392d0e8477b90dc1edb1ec
0x1DEA <seebeyondedm@gmail.com>
2024-11-13 22:16:45 -0600

    Add automatic year to footer

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {Link} from '@inertiajs/vue3'
 import route from "ziggy-js";
+import Tooltip from "@/Components/Tooltip.vue";
 </script>
 <template>
     <div class="y z-10 items-center w-full bg-ui-1000">
@@ -16,13 +17,13 @@ import route from "ziggy-js";
         </div>
         <div class="x justify-center bg-ui-950 p-2 w-full text-sm text-ui-600">
             <div class="x justify-between lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl w-full">
-                <span>Hyperbolus &copy; 2023</span>
+                <span>Hyperbolus &copy; {{ new Date().getFullYear() }}</span>
                 <div class="x items-center space-x-2">
                     <span class="relative inline-flex h-2 w-2">
                          <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                          <span class="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
                     </span>
-                    <span>Connected via Alakazam</span>
+                    <span>Connected via <Tooltip message="I named my servers after different pokémon :P" inline>Alakazam</Tooltip></span>
                 </div>
             </div>
         </div>
