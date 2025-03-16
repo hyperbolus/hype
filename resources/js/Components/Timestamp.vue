c21ff630af44aa6b4dd01eb5d8930486b5eab0ab
0x1DEA <seebeyondedm@gmail.com>
2025-03-15 23:29:53 -0500

    refactor timestamp component

--- resources/js/Components/Timestamp.vue
+++ resources/js/Components/Timestamp.vue
@@ -1,79 +1,13 @@
 <script setup>
 import Tooltip from "@/Components/Tooltip.vue";
 import {onBeforeMount, ref} from "vue";
+import {useTimeAgo} from "@vueuse/core";
 
 const props = defineProps({
     time: [String, Number],
-    length: Boolean
+    length: Boolean,
+    position: String
 })
-
-// https://muffinman.io/blog/javascript-time-ago-function/
-const MONTH_NAMES = [
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
-];
-
-const getFormattedDate = (date, prefomattedDate = false, hideYear = false) => {
-    const day = date.getDate();
-    const month = MONTH_NAMES[date.getMonth()];
-    const year = date.getFullYear();
-    const hours = date.getHours();
-    let minutes = date.getMinutes();
-
-    if (minutes < 10) {
-        // Adding leading zero to minutes
-        minutes = `0${ minutes }`;
-    }
-
-    if (prefomattedDate) {
-        // Today at 10:20
-        // Yesterday at 10:20
-        return `${ prefomattedDate } at ${ hours }:${ minutes }`;
-    }
-
-    if (hideYear) {
-        // 10. January at 10:20
-        return `${ day }, ${ month } at ${ hours }:${ minutes }`;
-    }
-
-    // 10. January 2017. at 10:20
-    return `${ day }, ${ month } ${ year }, at ${ hours }:${ minutes }`;
-}
-
-const timeAgo = (dateParam) => {
-    if (!dateParam) {
-        return null;
-    }
-
-    const date = typeof dateParam === 'object' ? dateParam : new Date(dateParam);
-    const DAY_IN_MS = 86400000; // 24 * 60 * 60 * 1000
-    const today = new Date();
-    const yesterday = new Date(today - DAY_IN_MS);
-    const seconds = Math.round((today - date) / 1000);
-    const minutes = Math.round(seconds / 60);
-    const isToday = today.toDateString() === date.toDateString();
-    const isYesterday = yesterday.toDateString() === date.toDateString();
-    const isThisYear = today.getFullYear() === date.getFullYear();
-
-
-    if (seconds < 5) {
-        return 'now';
-    } else if (seconds < 60) {
-        return `${ seconds } seconds ago`;
-    } else if (seconds < 90) {
-        return 'about a minute ago';
-    } else if (minutes < 60) {
-        return `${ minutes } minutes ago`;
-    } else if (isToday) {
-        return getFormattedDate(date, 'Today,'); // Today at 10:20
-    } else if (isYesterday) {
-        return getFormattedDate(date, 'Yesterday,'); // Yesterday at 10:20
-    } else if (isThisYear) {
-        return getFormattedDate(date, false, true); // 10. January at 10:20
-    }
-
-    return getFormattedDate(date); // 10. January 2017. at 10:20
-}
 
 const timeLength = (seconds) => {
     if (!seconds) {
@@ -123,7 +57,7 @@ const timeLength = (seconds) => {
 const result = ref(null)
 
 onBeforeMount(() => {
-    result.value = props.length ? timeLength(props.time) : timeAgo(props.time)
+    result.value = props.length ? timeLength(props.time) : useTimeAgo(props.time)
 })
 
 </script>
@@ -131,7 +65,7 @@ onBeforeMount(() => {
     <template v-if="length">
         {{ result }}
     </template>
-    <Tooltip v-else class="select-none inline-block" :message="time">
+    <Tooltip v-else :position="position" class="select-none inline-block" container-class="text-ui-400" :message="new Date(time).toLocaleString()">
         {{ result }}
     </Tooltip>
 </template>
