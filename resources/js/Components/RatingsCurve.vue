d5caa5ba2e72bafb2d1096fc2fa97e3b9214a8d1
0x1DEA <seebeyondedm@gmail.com>
2025-04-03 22:23:07 -0500

    move rating curve tooltip to the right so it's not cut off

--- resources/js/Components/RatingsCurve.vue
+++ resources/js/Components/RatingsCurve.vue
@@ -9,7 +9,7 @@ const props = defineProps({
     <div class="y pane !pl-2">
         <h1 class="px-2">Rating Curve</h1>
         <div class="flex flex-col-reverse">
-            <Tooltip v-for="(count, score) in curve" position="left" :message="`${count} Ratings`">
+            <Tooltip v-for="(count, score) in curve" position="right" :message="`${count} Ratings`">
                 <div class="x items-center">
                     <span class="text-sm text-ui-500 w-5 mr-2 text-center">{{ score }}</span>
                     <div class="bg-ui-800 rounded overflow-hidden grow">
