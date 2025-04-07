a12a002db73a970c9aa3d027a9ab72cced7f714e
0x1DEA <seebeyondedm@gmail.com>
2025-04-07 02:56:43 -0500

    add playlist card component

---
+++ resources/js/Components/PlaylistTicket.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {Link} from '@inertiajs/vue3'
+import route from "ziggy-js";
+
+const props = defineProps({
+    playlist: Object
+});
+</script>
+<template>
+    <Link :href="route('playlists.show', playlist.id)" class="y pane">
+        <h1 class="font-bold text-xl">{{ playlist.title }}</h1>
+        <p>{{ playlist.description }}</p>
+    </Link>
+</template>
