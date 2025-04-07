d7399c33c1aa3a39147bcc762ce6d9c3dbe99703
0x1DEA <seebeyondedm@gmail.com>
2025-04-07 18:15:25 -0500

    emit event when slider drag is finished

--- resources/js/Components/MinMaxInput.vue
+++ resources/js/Components/MinMaxInput.vue
@@ -10,7 +10,7 @@ const props = defineProps({
     high: Number
 });
 
-const emit = defineEmits(['update:low', 'update:high'])
+const emit = defineEmits(['update:low', 'update:high', 'commit'])
 const a = ref(props.low);
 const b = ref(props.high);
 const minV = computed(() => Math.min(a.value, b.value));
@@ -20,6 +20,10 @@ const update = () => {
     emit('update:low', minV.value);
     emit('update:high', maxV.value);
 }
+
+const commit = () => {
+    emit('commit');
+}
 </script>
 <template>
     <div class="y space-y-1 w-full">
@@ -28,8 +32,8 @@ const update = () => {
             <span>{{ maxV }}</span>
         </div>
         <div class="relative w-full pb-2">
-            <input v-if="!disabled" class="w-full z-20 absolute h-1 bg-transparent pointer-events-none" v-model="a" @input="update" :step="step" :min="min" :max="max" type="range" :disabled="disabled"/>
-            <input v-if="!disabled" class="w-full z-20 absolute h-1 bg-transparent pointer-events-none" v-model="b" @input="update" :step="step" :min="min" :max="max" type="range"  :disabled="disabled"/>
+            <input @keyup.enter="commit" @mouseup="commit" @touchend="commit" v-if="!disabled" class="w-full z-20 absolute h-1 bg-transparent pointer-events-none" v-model="a" @input="update" :step="step" :min="min" :max="max" type="range" :disabled="disabled"/>
+            <input @keyup.enter="commit" @mouseup="commit" @touchend="commit" v-if="!disabled" class="w-full z-20 absolute h-1 bg-transparent pointer-events-none" v-model="b" @input="update" :step="step" :min="min" :max="max" type="range"  :disabled="disabled"/>
             <div class="z-10 relative rounded-full bg-ui-950 px-2 overflow-hidden">
                 <div class="p-0.5 bg-blue-500 rounded-full" :class="{'invisible': disabled}" :style="`width:${(maxV - minV) / (max - min) * 100}%;margin-left:${(minV - min) / (max - min) * 100}%;`"></div>
             </div>
