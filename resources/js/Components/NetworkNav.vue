a2c3fc1308eeea32d4ca895a6feeec62f9a85d2d
0x1DEA <seebeyondedm@gmail.com>
2025-04-07 21:29:20 -0500

    add soundodger to network navbar

--- resources/js/Components/NetworkNav.vue
+++ resources/js/Components/NetworkNav.vue
@@ -1,12 +1,9 @@
-<script setup>
-import {ref} from "vue";
-</script>
 <template>
     <div class="x z-30 hidden transition-colors justify-center w-full bg-ui-950">
         <div class="hidden md:flex justify-between w-full items-center lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl w-full text-xs">
             <div class="x divide-x divide-ui-900">
                 <a class="px-2 py-1 transition-colors bg-ui-900 border-l border-ui-900">Geometry Dash</a>
-                <a href="https://soundodger-community.com" class="hidden px-2 py-1 transition-colors hover:bg-ui-800">Soundodger</a>
+                <a href="https://soundodger-community.com" class="px-2 py-1 transition-colors hover:bg-ui-800">Soundodger</a>
                 <a href="https://userlevels.com/impossible" class="px-2 py-1 transition-colors hover:bg-ui-800 border-r border-ui-900">The Impossible Game 2</a>
             </div>
         </div>
