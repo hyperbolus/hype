d1c4a4ada812693b82f7cc0c668425e2ceb4e507
0x1DEA <seebeyondedm@gmail.com>
2025-04-10 20:42:55 -0500

    layout container positioning

--- resources/js/Layouts/Dash.vue
+++ resources/js/Layouts/Dash.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {onBeforeMount} from 'vue';
-import {Head} from '@inertiajs/vue3';
+import {Head, Link} from '@inertiajs/vue3';
 import NetworkNav from "@/Components/NetworkNav.vue";
 import SiteNav from "@/Components/SiteNav.vue";
 import Breadcrumbs from "@/Components/Breadcrumbs.vue";
@@ -8,6 +8,8 @@ import Footer from "@/Components/Footer.vue";
 import patternBG from "@/../images/card_background_outline.svg"
 import {useSettingsStore} from "@/stores/settings.ts";
 import {useStatisticsStore} from "@/stores/statistics.ts";
+import route from "ziggy-js";
+import {isAuthenticated} from "@/util.js";
 
 const props = defineProps({
     title: String,
@@ -46,10 +48,16 @@ onBeforeMount(() => {
             <Breadcrumbs :fullwidth="fullwidth"><slot name="breadcrumbs"/></Breadcrumbs>
             <div v-if="background" class="absolute -z-10 h-full w-full bg-ui-900" style="mask-size: 8rem;" :style="`mask-image: url('${patternBG}?');`"></div>
             <div class="absolute -z-20 h-full w-full bg-ui-1000"></div>
-            <div v-if="decorations" class="flex flex-col flex-grow shadow-xl bg-ui-950 border border-ui-900/75 md:flex-row lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl w-full gap-4 p-4 my-4 rounded-lg">
-                <slot/>
+            <div v-if="decorations" class="x justify-center w-full my-4">
+                <div class="flex flex-col flex-grow shadow-xl bg-ui-950 border-y md:border-x border-ui-900/75 md:flex-row lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl w-full gap-4 p-2 md:p-4 md:mx-4 md:rounded-lg">
+                    <slot/>
+                </div>
             </div>
             <slot v-else/>
+            <div v-if="!isAuthenticated()" class="mb-2">
+                <div class="w-[728px] h-[90px] bg-blue-500"></div>
+                <span class="text-sm">Don't want to see ads? <Link class="underline" :href="route('auth::login')">Sign in.</Link></span>
+            </div>
         </div>
         <Footer/>
     </div>
