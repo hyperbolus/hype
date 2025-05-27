66d8915d23c856041612b5462c6b58ab087deba7
0x1DEA <seebeyondedm@gmail.com>
2025-05-26 19:20:28 -0500

    fix RatingOverview.vue component growing improperly

--- resources/js/Components/RatingOverview.vue
+++ resources/js/Components/RatingOverview.vue
@@ -6,7 +6,7 @@ const props = defineProps({
 });
 </script>
 <template>
-    <div class="x pane justify-between space-x-4 grow">
+    <div class="x pane justify-between space-x-4">
         <div class="y">
             <span class="text-xs uppercase">Difficulty</span>
             <span class="text-xl font-bold">{{ displayRating(level.rating_difficulty, 2) }}<span class="text-xs text-ui-600">/100</span></span>
