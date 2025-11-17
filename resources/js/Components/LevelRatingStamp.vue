e2ba782d928556dff4da0fed3c20ddfc36a81e36
0x1DEA <seebeyondedm@gmail.com>
2025-11-17 17:28:08 -0600

    handle LevelRatingStamp.vue ui when there's no review count

--- resources/js/Components/LevelRatingStamp.vue
+++ resources/js/Components/LevelRatingStamp.vue
@@ -31,9 +31,9 @@ const list = {
                             <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
                         </svg>
                     </div>
-                    <span class="text-lg font-bold">{{ level.reviews_count }}</span>
+                    <span class="text-lg font-bold">{{ level.reviews_count ?? 'N/A' }}</span>
                 </Tooltip>
-                <div v-else class="y">
+                <div v-else-if="level.reviews_count" class="y">
                     <span class="text-xs uppercase">Reviews</span>
                     <span class="text-lg font-bold">{{ level.reviews_count }}</span>
                 </div>
