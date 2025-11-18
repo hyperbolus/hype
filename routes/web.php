03029182b6c21631c9ad9b773b41e89fdc6ebda7
0x1DEA <seebeyondedm@gmail.com>
2025-11-17 20:07:42 -0600

    remove old profilegd route

--- routes/web.php
+++ routes/web.php
@@ -55,11 +55,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::domain(config('app.domains.profile'))->group(function () {
-    Route::redirect('/', config('app.url'));
-    Route::get('/{profile}', [ProfileController::class, 'show']);
-});
-
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/about', [HomeController::class, 'about'])->name('about');
 Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
