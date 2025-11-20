a0f34a36a0f42bbf2970ee54e0d505e06c93d97a
0x1DEA <seebeyondedm@gmail.com>
2025-11-19 22:15:31 -0600

    fix spoiler to work multiline

--- resources/js/Components/TipTap/Spoiler.vue
+++ resources/js/Components/TipTap/Spoiler.vue
@@ -8,7 +8,6 @@ const visible = ref(false);
 </script>
 <template>
     <span @click="visible = !visible" class="w-fit cursor-pointer relative rounded bg-ui-700 p-0.5">
-        <span v-show="!visible" class="absolute inset-0 bg-ui-500 rounded"></span>
-        <mark-view-content as="span" class="transition-[filter]"/>
+        <mark-view-content as="span" class="rounded select-none" :class="{'text-transparent bg-ui-500': !visible}"/>
     </span>
 </template>
