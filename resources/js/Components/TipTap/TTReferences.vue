da1bf42231b986d07a2293c0d62c80b30cc4fcaa
0x1DEA <seebeyondedm@gmail.com>
2026-03-25 16:57:36 -0500

    fix footnote references list opening anchors in new tabs instead of scrolling

--- resources/js/Components/TipTap/TTReferences.vue
+++ resources/js/Components/TipTap/TTReferences.vue
@@ -11,7 +11,8 @@ const footnotes = computed(() => props.editor.reactiveExtensionStorage.value.foo
     <NodeViewWrapper contenteditable="false" :class="{'border border-ui-700 px-2 py-1 rounded': editing}">
         <h1>{{ props.HTMLAttributes.title ?? 'Notes' }}</h1>
         <ol>
-            <li v-for="(note, name) in footnotes[props.HTMLAttributes.group]">{{ name }} // {{ note.content }} <a :href="`#fn:${note.id + 1}`">^{{ note.id + 1 }}</a></li>
+            <!-- For some reason, this anchor link needs an explicit target="_self" or else it will open in a new tab (??) -->
+            <li v-for="(note, name) in footnotes[props.HTMLAttributes.group]">{{ name }} // {{ note.content }} <a target="_self" :href="`#fn:${note.id + 1}`">^{{ note.id + 1 }}</a></li>
         </ol>
     </NodeViewWrapper>
 </template>
