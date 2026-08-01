9d743c886f66a4df96fb400b94384de7e12b926c
0x1DEA <idea@metasigil.com>
2026-08-01 01:44:40 -0500

    Level roulette redesign

--- app/Http/Controllers/Game/RouletteController.php
+++ app/Http/Controllers/Game/RouletteController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Game;
 use App\Http\Controllers\Controller;
 use App\Models\Content\Playlist;
 use Illuminate\Http\Request;
-use Inertia\Inertia;
 
 class RouletteController extends Controller
 {
@@ -14,22 +13,21 @@ class RouletteController extends Controller
         $levels = [];
         $playlist = null;
 
-        if (! $request->has('seed')) {
-            return redirect($request->fullUrlWithQuery(['seed' => mt_rand(10000, 99999)]));
-        }
+        if (!$request->has('seed')) return redirect($request->fullUrlWithQuery(['seed' => mt_rand(10000, 99999)]));
 
         if ($request->string('type') == 'playlist') {
             $playlist = Playlist::query()->with(['owner', 'levels' => function ($query) use ($request) {
-                $query->inRandomOrder($request->integer('seed'));
+                $query->inRandomOrder($request->integer('seed'))->with(['videos']);
             }])->findOrFail($request->integer('id'));
+
             $levels = $playlist->levels;
             $playlist = $playlist->makeHidden('levels');
         }
 
-        return Inertia::render('Roulette', [
+        return page('Roulette', [
             'levels' => $levels,
             'playlist' => $playlist,
             'progress' => $request->integer('progress'),
-        ]);
+        ])->meta('Roulette', 'Play level roulette');
     }
 }
--- resources/js/Pages/Roulette.vue
+++ resources/js/Pages/Roulette.vue
@@ -3,16 +3,27 @@ import AppLayout from "@/Layouts/Dash.vue";
 import {ref} from "vue";
 import {Link, usePage} from '@inertiajs/vue3'
 import Username from "@/Components/Username.vue";
+import LevelTicket from "@/Components/LevelTicket.vue";
+import PlaylistTicket from "@/Components/PlaylistTicket.vue";
+import Input from "@/Jetstream/Input.vue";
+import VideoLightbox from "@/Components/VideoLightbox.vue";
+import {isAdmin} from "@/util";
 
 const props = defineProps({
     levels: Array,
     progress: Number,
     playlist: Object
-})
+});
+
+// TODO: add roulette main page if no playlist selected
 
 const progress = ref(props.progress)
+const percent = ref(0);
+const currentPercent = ref(null);
 const finished = ref(false)
 const won = ref(false)
+
+const levelPercents = ref([]);
 
 const newSeed = () => {
     let params = new URLSearchParams(usePage().url)
@@ -21,50 +32,109 @@ const newSeed = () => {
 }
 
 const next = () => {
+    if (currentPercent.value <= percent.value || currentPercent.value % 1 !== 0) {
+        alert(`Invalid percent. Must be higher than ${percent.value + 1}. Whole numbers only!`)
+        return;
+    }
+
+    levelPercents.value[progress.value] = currentPercent.value;
+    percent.value = currentPercent.value;
+
+    currentPercent.value++;
+
     if (++progress.value === props.levels.length) {
+        // you died ran out of levels somehow
+        finished.value = true;
+        won.value = false;
+    }
+
+    if (percent.value >= 100) {
         finished.value = true;
         won.value = true;
     }
 }
+
+const reset = () => {
+    progress.value = 0;
+    finished.value = false;
+    won.value = false;
+    percent.value = 0
+    currentPercent.value = null;
+    levelPercents.value = [];
+}
 </script>
 <template>
-    <app-layout title="Roulette">
-        <template #breadcrumbs>
-            <Link :href="$page.url">Roulette</Link>
-        </template>
-        <div class="y space-y-2 md:w-2/3">
-            <h1 class="mx-2 text-center font-bold text-4xl">{{ finished ? 'Results' : 'Level Roulette'}}</h1>
-            <div class="x self-center space-x-2">
-                <span class="pane !py-1"><Link :href="route('playlists.show', playlist.id)">{{ playlist.title }}</Link> by <Username :user="playlist.owner"/></span>
-                <span class="pane !py-1" v-show="!finished">Current Progress: {{progress }} {{ levels.length }}</span>
+    <app-layout>
+        <div class="y space-y-2 w-full">
+            <teleport to="#teleports">
+                <div v-if="!(finished && won)" class="fixed inset-0 z-50 pointer-events-none" :style="`box-shadow:inset 0 0 200px 0 #FF000060;background:radial-gradient(transparent 80%, #ff803020);opacity:${progress / levels.length};`"></div>
+            </teleport>
+
+            <PlaylistTicket :playlist="playlist"/>
+            <h1 class="mx-2 text-center font-bold text-4xl mb-2">{{ finished ? 'Results' : 'Level Roulette'}}</h1>
+            <div v-if="!finished" class="grid md:grid-cols-2 gap-4">
+                <div class="y space-y-2">
+                    <VideoLightbox v-if="levels[progress].videos.length > 0" :video="levels[progress].videos[0]"/>
+                    <div v-else class="y justify-center text-center aspect-video bg-ui-1000 border-2 border-dashed border-ui-700 w-full rounded-lg">
+                        <span>No video available</span>
+                    </div>
+                </div>
+                <div class="y justify-between items-center space-y-2">
+                    <LevelTicket :level="levels[progress]" :show-ratings="false" class="w-full"/>
+                    <div class="y items-center space-y-2">
+                        <span class="font-bold text-xl">At least {{ percent + 1 }}%</span>
+                        <Input :max="100" :step="1" type="number" v-model.number="currentPercent" class="!bg-ui-800" :placeholder="`At least ${percent + 1}%`"/>
+                        <div class="x space-x-2 justify-center text-while font-bold items-center w-full">
+                            <button class="bg-blue-500 px-4 py-2 rounded-lg" @click="next">Done</button>
+                            <button class="bg-ui-800 px-4 py-2 rounded-lg" @click="finished = true">Give Up</button>
+                        </div>
+                        <span v-if="isAdmin()" class="pane !py-1 cursor-pointer" @click="reset">Reset</span>
+                        <Link :href="newSeed()" class="text-blue-500 text-sm hover:underline">New Seed</Link>
+                    </div>
+                    <div class="w-full">
+                        <div class="x justify-between text-sm">
+                            <span>{{ percent }}%</span>
+                            <span>100%</span>
+                        </div>
+                        <div class="y space-y-0.5 bg-ui-900 px-1 rounded-full">
+                            <div class="bg-blue-500 py-0.5 rounded-t-full" :style="`width: ${percent}%;`"></div>
+                            <div class="bg-red-500 py-0.5 rounded-b-full" :style="`width: ${100 - progress / levels.length * 100}%;`"></div>
+                        </div>
+                        <div class="x justify-between text-sm">
+                            <span>{{ levels.length - progress }}/{{levels.length}} Levels Remaining</span>
+                            <span>Level {{ progress + 1 }}</span>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <template v-if="finished">
+
+            <div v-if="finished">
                 <div class="y space-y-2 text-center">
-                    <span class="pane !py-1 self-center">{{ progress }} Level{{ progress === 1 ? '' : 's'}} Passed</span>
+                    <span v-if="won" class="pane !py-1 self-center">Win in {{ progress }} level{{ progress === 1 ? '' : 's'}} (out of {{ levels.length }})</span>
+                    <span v-else class="pane !py-1 self-center">Got {{ percent }}% in {{ progress }} level{{ progress === 1 ? '' : 's'}}</span>
                     <span class="font-bold text-xl" :class="won ? 'text-green-500' : 'text-red-500'">YOU {{ won ? 'WIN!' : 'LOSE...' }}</span>
+                    <template v-if="!won">
+                        <span v-if="progress === levels.length">You ran out of levels!</span>
+                        <span v-else>You gave up</span>
+                    </template>
                     <div class="x justify-center space-x-2">
-                        <span class="pane !py-1 cursor-pointer" @click="progress = 0; finished = false; won = false">Reset</span>
+                        <span v-if="isAdmin()" class="pane !py-1 cursor-pointer" @click="reset">Reset</span>
                         <Link :href="newSeed()" class="pane !py-1 cursor-pointer">New Seed</Link>
                     </div>
                 </div>
-            </template>
-            <template v-else>
-                <div v-for="(level, index) in levels" :key="index" v-show="index <= progress" class="x pane justify-between">
-                    <div class="x items-center space-x-2">
-                        <span class="font-bold text-xl">{{ level.name }}</span>
-                        <span class="text-sm">by {{ level.creator }}</span>
-                    </div>
-                    <div class="x space-x-2 items-center" v-show="index === progress">
-                        <span>At least {{ index+1 }}%</span>
-                        <button class="bg-ui-800 px-2 py-1 rounded" @click="next">Done</button>
-                        <button class="bg-ui-800 px-2 py-1 rounded" @click="finished = true">Give Up</button>
-                    </div>
+            </div>
+            <div class="flex flex-col-reverse gap-1">
+                <div v-for="(level, index) in levels" class="x space-x-2 items-center w-full" v-show="index < progress">
+                    <span class="rounded-lg bg-ui-900 py-1 font-bold text-center w-16">{{ levelPercents[index] ?? '?' }}%</span>
+                    <LevelTicket :key="index" :level="level" :show-ratings="false" class="grow"/>
                 </div>
-            </template>
-        </div>
-        <div class="y space-y-2 md:w-1/3">
-            <h1 class="mx-2 font-bold text-2xl">About</h1>
-            <p class="pane">This game was inspired by <a class="underline" href="https://github.com/matcool/" target="_blank">matcool</a>'s <a class="underline" href="https://matcool.github.io/extreme-demon-roulette/" target="_blank">Extreme Demon Roulette</a> website with the original idea by <a class="underline" href="https://youtu.be/nv_9FkfGRsc" target="_blank">npesta</a></p>
+                <p v-if="progress === 0" class="rounded-lg bg-ui-1000 text-center px-2 py-2 text-ui-500">You haven't passed any levels</p>
+                <h1 class="mx-2 text-center font-bold text-4xl my-2">Passed</h1>
+            </div>
+            <div class="y space-y-2 text-center">
+                <h1 class="mx-2 font-bold text-2xl">About</h1>
+                <p class="pane">This game was inspired by <a class="underline" href="https://github.com/matcool/" target="_blank">matcool</a>'s <a class="underline" href="https://matcool.github.io/extreme-demon-roulette/" target="_blank">Extreme Demon Roulette</a> website with the original idea by <a class="underline" href="https://youtu.be/nv_9FkfGRsc" target="_blank">npesta</a></p>
+            </div>
         </div>
     </app-layout>
 </template>
